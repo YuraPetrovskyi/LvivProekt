b29f03f76c6b6c490a2161e2e58f92793ecacffa
Yurii Petrovskyi <yurakarpaty@gmail.com>
2024-05-30 18:46:15 +0100

    added supabase client

--- server/server.js
+++ server/server.js
@@ -1,32 +1,46 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { pool } = require('../server/db');
+require('dotenv').config();
+// const { pool } = require('./db/pool');
+
+const { createClient } = require('@supabase/supabase-js');
+
+const supabaseUrl = process.env.supabaseUrl;
+const supabaseKey = process.env.supabaseKey;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-// const pool = new Pool({
-//   user: 'yourusername',
-//   host: 'localhost',
-//   database: 'yourdatabase',
-//   password: 'yourpassword',
-//   port: 5432,
-// });
-
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get('/api/projects', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM projects');
-    res.json(result.rows);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: 'Internal Server Error' });
+
+app.get('/api/contacts', async (req, res) => {
+  const { data, error } = await supabase
+    .from('contacts')
+    .select('*');
+
+  if (error) {
+    return res.status(500).json({ error: error.message });
   }
+
+  res.json(data);
 });
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+
+// app.get('/api/projects', async (req, res) => {
+//   try {
+//     console.log('/api/projects')
+//     const result = await pool.query('SELECT * FROM projects');
+//     res.json(result.rows);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
